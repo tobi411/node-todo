408c89fec3f884cfd8fbbd0a9410d82687f39721
Tobi Kehinde <tokehinde11@gmail.com>
2015-11-17 06:07:16 -0700

    notes, note and app components implemented and the button to add note added

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,6 +1,45 @@
+import uuid from 'node-uuid';
 import React from "react";
-import Note from './Note.jsx';
+import Notes from './Notes.jsx';
 
-export default () => {
-    return <Note />;
-};
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            notes: [
+                {
+                    id: uuid.v4(),
+                    task: 'Learn Webpack'
+                },
+                {
+                    id: uuid.v4(),
+                    task: 'Learn React'
+                },
+                {
+                    id: uuid.v4(),
+                    task: 'Do laundry'
+                }
+            ]
+        };
+    }
+
+    render() {
+        const notes = this.state.notes;
+        return (
+            <div>
+                <button className="add-note" onClick={this.addNote.bind(this)}>+</button>
+                <Notes items={notes} />
+            </div>
+        );
+    }
+
+    addNote() {
+        this.setState({
+            notes: this.state.notes.concat([{
+                id: uuid.v4(),
+                task: 'New task'
+            }])
+        });
+    }
+}
--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -3,10 +3,9 @@ import React from 'react';
 export default class Note extends React.Component {
     render() {
         return (
-            <div>
-                <h1>Learn Webpack</h1>
-                <p>It's been cool but rather daunting to set up. Things are getting interesting.</p>
-            </div>
+             <div>
+                 {this.props.task}
+             </div>
         );
     }
 }
